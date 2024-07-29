3575a18ce6b8e2c7f559dbb40bc84717980af520
Your Name <you@example.com>
2024-07-30 00:09:31 +0530

    Deployed server api

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const app = express();
 const port = process.env.PORT_NUMBER || 8000;
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin:process.env.CLIENT_URL,
     credentials: true 
 };
 
