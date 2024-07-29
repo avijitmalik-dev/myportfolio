9ab612f213b495ed7bd5b2de97478fc9d7f80b92
Your Name <you@example.com>
2024-07-29 23:58:30 +0530

    add deployed api

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,7 +1,7 @@
 import  axios  from 'axios';
 
 const apiConfig = axios.create(({
-    baseURL : "http://localhost:8000",
+    baseURL : "https://myportfolio-backend-five.vercel.app",
     withCredentials : true
 }));
 
